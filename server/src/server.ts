d17048472cfdc7585b82c66dc55291b8dc59b513
sashi0034 <satoshi3104.nishi@gmail.com>
2024-04-03 12:54:24 +0900

    設定項目の部分を修正

--- server/src/server.ts
+++ server/src/server.ts
@@ -94,6 +94,12 @@ connection.onInitialize((params: InitializeParams) => {
     return result;
 });
 
+function loadSettings() {
+    connection.workspace.getConfiguration('angelScript').then((config) => {
+        globalSettings = <LanguageServerConfigurations>(config || defaultSettings);
+    });
+}
+
 connection.onInitialized(() => {
     if (hasConfigurationCapability) {
         // Register for all configuration changes.
@@ -104,51 +110,38 @@ connection.onInitialized(() => {
             connection.console.log('Workspace folder change event received.');
         });
     }
+
+    // ワークスペース設定の読み込み
+    loadSettings();
 });
 
-// The example settings
-interface ExampleSettings {
+interface LanguageServerConfigurations {
     maxNumberOfProblems: number;
+    trace: {
+        server: 'off' | 'messages' | 'verbose';
+    };
 }
 
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
 // Please note that this is not the case when using this server with the client provided in this example
 // but could happen with other clients.
-const defaultSettings: ExampleSettings = {maxNumberOfProblems: 1000};
-let globalSettings: ExampleSettings = defaultSettings;
+const defaultSettings: LanguageServerConfigurations = {
+    maxNumberOfProblems: 1000,
+    trace: {
+        server: 'off'
+    }
+};
+
+let globalSettings: LanguageServerConfigurations = defaultSettings;
 
 // Cache the settings of all open documents
-const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
+const documentSettings: Map<string, Thenable<LanguageServerConfigurations>> = new Map();
 
 connection.onDidChangeConfiguration(change => {
-    if (hasConfigurationCapability) {
-        // Reset all cached document settings
-        documentSettings.clear();
-    } else {
-        globalSettings = <ExampleSettings>(
-            (change.settings.languageServerExample || defaultSettings)
-        );
-    }
-    // Refresh the diagnostics since the `maxNumberOfProblems` could have changed.
-    // We could optimize things here and re-fetch the setting first can compare it
-    // to the existing setting, but this is out of scope for this example.
+    loadSettings();
+
     connection.languages.diagnostics.refresh();
 });
-
-function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
-    if (!hasConfigurationCapability) {
-        return Promise.resolve(globalSettings);
-    }
-    let result = documentSettings.get(resource);
-    if (!result) {
-        result = connection.workspace.getConfiguration({
-            scopeUri: resource,
-            section: 'languageServerExample'
-        });
-        documentSettings.set(resource, result);
-    }
-    return result;
-}
 
 // Only keep settings for open documents
 documents.onDidClose(e => {
@@ -220,6 +213,10 @@ connection.onRenameRequest((params) => {
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(change => {
+    if (globalSettings.trace.server === 'verbose') {
+        connection.console.log(`Document got changed: ${change.document.uri}`);
+    }
+
     inspectFile(change.document.getText(), getDocumentPath(change));
 });
 
