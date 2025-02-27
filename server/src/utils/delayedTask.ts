a15456ee694811e817294e437a3f12b1ab178fd8
sashi <satoshi3104.nishi@gmail.com>
2025-02-27 18:21:25 +0900

    Add DelayedTask

---
+++ server/src/utils/delayedTask.ts
@@ -0,0 +1,15 @@
+/**
+ * A class that allows for a task to be rescheduled with a delay.
+ */
+export class DelayedTask {
+    private _timer: NodeJS.Timeout | undefined = undefined;
+
+    public reschedule(callback: (args: void) => void, delay?: number) {
+        if (this._timer !== undefined) {
+            clearTimeout(this._timer);
+        }
+
+        this._timer = setTimeout(callback, delay);
+    }
+}
+
