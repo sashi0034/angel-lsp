0173803ba06343d2ac87e204b86d3d98cbddbea1
sashi <satoshi3104.nishi@gmail.com>
2025-03-17 17:39:06 +0900

    Add tests

--- server/test/compiler/analyzer.spec.ts
+++ server/test/compiler/analyzer.spec.ts
@@ -70,7 +70,28 @@ describe("Analyzer", () => {
         }
     `);
 
-    // TODO: Add more tests
+    // Named arguments
+    expectSuccess(`
+        class B { }
+        class C { }
+
+        void foo(int a, B b = B(), C c = C(), bool d = false, double e = 0) { }
+        
+        void main() {
+            foo(1, e: 2.0, b: B(), d: true);
+        }
+    `);
+
+    expectError(`
+        class B { }
+        class C { }
+
+        void foo(int a, B b = B(), C c = C(), bool d = false, double e = 0) { }
+        
+        void main() {
+            foo(e: 2.0, 1, b: B(), d: true); // Positional arguments cannot be passed after named arguments
+        }
+    `);
 });
 
 
