7e690c696607801369d3b3058a6bfbf321bcff8c
sashi <satoshi3104.nishi@gmail.com>
2025-03-19 15:31:22 +0900

    Completion resolve for types and variables

--- server/src/services/completionResolve.ts
+++ server/src/services/completionResolve.ts
@@ -1,37 +1,73 @@
-import {SymbolGlobalScope, SymbolScope} from "../compiler_analyzer/symbolScope";
-import {CompletionItem, CompletionItemKind} from "vscode-languageserver/node";
-import {stringifySymbolObject} from "../compiler_analyzer/symbolUtils";
+import {SymbolGlobalScope} from "../compiler_analyzer/symbolScope";
+import {CompletionItem} from "vscode-languageserver/node";
+import {stringifyResolvedType, stringifySymbolObject} from "../compiler_analyzer/symbolUtils";
 import {InsertTextFormat} from "vscode-languageserver";
-import {SymbolFunctionHolder} from "../compiler_analyzer/symbolObject";
+import {SymbolFunctionHolder, SymbolType, SymbolVariable} from "../compiler_analyzer/symbolObject";
 import {CompletionItemWrapper} from "./completion";
-import {logger} from "../core/logger";
-import {getDocumentCommentOfSymbol} from "./utils";
+import {NodeName} from "../compiler_parser/nodes";
+import * as path from 'path';
 
 /**
  * Completion Resolve is invoked when the user sees the completion item.
  */
 export function provideCompletionResolve(
-    globalScope: SymbolGlobalScope, item: CompletionItemWrapper
+    globalScope: SymbolGlobalScope, itemWrapper: CompletionItemWrapper
 ): CompletionItem {
-    if (item.item.kind === CompletionItemKind.Function) {
-        return resolveFunctionItem(globalScope, item);
-    }
-
-    return item.item;
-}
-
-function resolveFunctionItem(globalScope: SymbolGlobalScope, itemWrapper: CompletionItemWrapper) {
     const item = itemWrapper.item;
     const symbol = itemWrapper.symbol;
-    if (symbol === undefined || symbol.isFunctionHolder() === false) {
-        logger.error(`Received item is not a function: ${item.label}`);
-        return item;
+    if (symbol?.isVariable()) {
+        return resolveVariableItem(item, symbol);
+    } else if (symbol?.isType()) {
+        return resolveTypeItem(globalScope, item, symbol);
+    } else if (symbol?.isFunctionHolder()) {
+        return resolveFunctionItem(item, symbol);
     }
 
+    return item;
+}
+
+// -----------------------------------------------
+
+function resolveVariableItem(item: CompletionItem, symbol: SymbolVariable) {
+    item.detail = stringifyResolvedType(symbol.type) + ' ' + symbol.identifierText;
+
+    return item;
+}
+
+// -----------------------------------------------
+
+function resolveTypeItem(globalScope: SymbolGlobalScope, item: CompletionItem, symbol: SymbolType): CompletionItem {
+    item.detail = getTypeNodeName(symbol);
+    item.detail += ' ' + [...symbol.scopePath, symbol.identifierText].join('::');
+
+    if (globalScope.getContext().filepath !== symbol.identifierToken.location.path) {
+        // Insert the file name for types defined in outer files
+        item.detail += ' ' + ` from ${path.basename(symbol.identifierToken.location.path)}`;
+    }
+
+    return item;
+}
+
+function getTypeNodeName(symbol: SymbolType) {
+    const nodeName = symbol.linkedNode?.nodeName;
+    if (nodeName === NodeName.Enum) return 'enum';
+    if (nodeName === NodeName.Class) return 'class';
+    if (nodeName === NodeName.Interface) return 'interface';
+    return 'type';
+}
+
+// -----------------------------------------------
+
+function resolveFunctionItem(item: CompletionItem, symbol: SymbolFunctionHolder) {
     const functionSymbol = symbol.first;
 
     // Display the signature, e.g. "void fn(int a, int b)"
     item.detail = stringifySymbolObject(functionSymbol);
+
+    if (functionSymbol.linkedNode.nodeName === NodeName.FuncDef) {
+        // We do not insert snippets for funcdef
+        return item;
+    }
 
     // Set the snippet, e.g. "set_cu" |--> autocomplete |--> "set_cursor($C$)"
     item.insertText = item.label + (hasFunctionArguments(symbol) ? '($0)' : `()$0`);
@@ -42,11 +78,11 @@ function resolveFunctionItem(globalScope: SymbolGlobalScope, itemWrapper: Comple
     item.command = {command: 'editor.action.triggerParameterHints', title: 'Trigger Signature Help Provider'};
     // TODO: What should I do for other IDEs?
 
-    // FIXME: This doesn't work, why? :(
-    item.documentation = {
-        kind: 'markdown',
-        value: getDocumentCommentOfSymbol(functionSymbol)
-    };
+    // FIXME: This doesn't work, why?
+    // item.documentation = {
+    //     kind: 'markdown',
+    //     value: getDocumentCommentOfSymbol(functionSymbol)
+    // };
 
     return item;
 }
