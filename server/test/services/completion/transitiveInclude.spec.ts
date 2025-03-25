dc8f8352f2f76f428742b0250e965a4161f1f230
sashi <satoshi3104.nishi@gmail.com>
2025-03-26 08:28:12 +0900

    Add test

--- server/test/services/completion/transitiveInclude.spec.ts
+++ server/test/services/completion/transitiveInclude.spec.ts
@@ -1,7 +1,6 @@
 import {testCompletion} from "./utils";
 
 describe('completion/transitiveInclude', () => {
-
     testCompletion([{
             uri: 'file:///path/to/file_1.as',
             content: `
@@ -15,12 +14,37 @@ describe('completion/transitiveInclude', () => {
         `
         }, {
             uri: 'file:///path/to/file_3.as',
-            content: `// Transitive includes are available
+            content: `// Transitive includes are available.
             #include "file_2.as"
             void main() {
                 $C0$
             }
         `
         }], /* $C0$ */ ["ThisYear", "GetNextYear", "main"]
+    );
+
+    testCompletion([{
+            uri: 'file:///path/to/file_1.as',
+            content: `
+            #include "file_1.as"
+            #include "file_2.as"
+            int Alpha = 1;
+            `
+        }, {
+            uri: 'file:///path/to/file_2.as',
+            content: `
+            #include "file_1.as"
+            #include "file_2.as"
+            int Beta = 2;
+        `
+        }, {
+            uri: 'file:///path/to/file_3.as',
+            content: `// Transitive includes can be cyclic.
+            #include "file_1.as"
+            void main() {
+                $C0$
+            }
+        `
+        }], /* $C0$ */ ["Alpha", "Beta", "main"]
     );
 });
