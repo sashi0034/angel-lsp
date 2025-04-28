0a9d54f7a9986c19e13260f812b4882f18e586c7
sashi <satoshi3104.nishi@gmail.com>
2025-04-28 17:25:21 +0900

    Add a test

--- server/test/compiler/analyzer/foreachStatement.spec.ts
+++ server/test/compiler/analyzer/foreachStatement.spec.ts
@@ -1,4 +1,4 @@
-import {expectSuccess} from "./utils";
+import {expectError, expectSuccess} from "./utils";
 
 describe('analyzer/foreachStatement', () => {
     expectSuccess([{
@@ -22,6 +22,29 @@ describe('analyzer/foreachStatement', () => {
 
                 foreach (const auto f : arr) {
                     sum += f ? 1 : 0;
+                }
+
+                return sum;
+            }`
+    }]);
+
+    expectError([{
+        uri: 'file:///path/to/as.predefined',
+        content: `
+            class array<T>{
+                uint opForBegin() const;
+                bool opForEnd(uint) const;
+                uint opForNext(uint) const;
+                const T& opForValue0(uint index) const;
+                uint opForValue1(uint index) const;
+            }`
+    }, {
+        uri: 'file:///path/to/file.as',
+        content: `// Cannot use foreach statement with too many variables.
+            int iterate(array<bool> arr) {
+                int sum;
+                foreach (const auto f, const auto i, const auto unknown : arr) {
+                    sum += f ? i : 0;
                 }
 
                 return sum;
