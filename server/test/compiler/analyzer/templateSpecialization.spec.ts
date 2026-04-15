cf7577d2941207ae5577a431af3b86496e229189
sashi <sashi0034.sashi@gmail.com>
2026-04-15 16:15:48 +0900

    Add a test

--- server/test/compiler/analyzer/templateSpecialization.spec.ts
+++ server/test/compiler/analyzer/templateSpecialization.spec.ts
@@ -214,4 +214,17 @@ describe('analyzer/templateSpecialization', () => {
             }
         ]);
     });
+
+    it('accepts array of typedef primitive matches array of original primitive', () => {
+        expectSuccess(`
+            typedef uint8 byte;
+
+            class array<T> { }
+
+            void main() {
+                array<byte> src;
+                array<uint8> dst = src;
+            }
+        `);
+    });
 });
