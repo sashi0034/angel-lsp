a9adab334ac6006d2e97ea179134dc39ff11bad8
sashi <sashi0034.sashi@gmail.com>
2026-04-16 16:36:08 +0900

    Simplify

--- server/src/compiler_tokenizer/tokenizer.ts
+++ server/src/compiler_tokenizer/tokenizer.ts
@@ -114,13 +114,8 @@ function consumeNumber(tokenizer: TokenizerState) {
         return NumberLiteral.Integer;
     }
 
-    // Fails if the token after a dot is another dot
-    if (tokenizer.next() === '.' && tokenizer.next(1) === '.') {
-        return NumberLiteral.Integer;
-    }
-
-    // Fails if the next tokens are '.f' or '.F'
-    if (tokenizer.next(0) === '.' && /^[fF]$/.test(tokenizer.next(1))) {
+    // Fails if a leading dot is not followed by a digit.
+    if (tokenizer.next() === '.' && isDigit(tokenizer, 1) === false) {
         return NumberLiteral.Integer;
     }
 
