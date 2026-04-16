ff1d62fb8c1cc665d6563d6ce09859d28f2fbdd3
sashi <sashi0034.sashi@gmail.com>
2026-04-16 16:22:36 +0900

    Fix the test

--- server/test/compiler/parser.spec.ts
+++ server/test/compiler/parser.spec.ts
@@ -141,18 +141,20 @@ describe('Parser', () => {
 
     it('parses exponential notation', () => {
         expectSuccess(`
-            double e0 = 1e10;
-            e0 = 1e+10;
-            e0 = 1e-10;
-            e0 = 1.5e10;
-            e0 = 1.5e+10;
-            e0 = 1.5e-10;
-            e0 = .5e10;
-            e0 = .5e+10;
-            e0 = .5e-10;
-            e0 = 1.E10;
-            e0 = 1.E+10;
-            e0 = 1.E-10;
+            void test() {
+                double e0 = 1e10;
+                e0 = 1e+10;
+                e0 = 1e-10;
+                e0 = 1.5e10;
+                e0 = 1.5e+10;
+                e0 = 1.5e-10;
+                e0 = .5e10;
+                e0 = .5e+10;
+                e0 = .5e-10;
+                e0 = 1.E10;
+                e0 = 1.E+10;
+                e0 = 1.E-10;
+            }
         `);
     });
 });
