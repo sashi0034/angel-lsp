373962ce591ec46c9c6a6edb7d486ecb149045d3
sashi <sashi0034.sashi@gmail.com>
2026-04-23 14:51:13 +0900

    Fix e2e.sh

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -3,4 +3,7 @@
 export CODE_TESTS_PATH="$(pwd)/client/out/test"
 export CODE_TESTS_WORKSPACE="$(pwd)/client/testFixture"
 
+rm -f "$(pwd)/client/out/test/completion.test.js" "$(pwd)/client/out/test/completion.test.js.map"
+rm -f "$(pwd)/client/out/test/diagnostics.test.js" "$(pwd)/client/out/test/diagnostics.test.js.map"
+
 node "$(pwd)/client/out/test/runTest"
